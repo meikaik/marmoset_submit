e7e3121a0f748ad0dd53f1716875d51fc32b5964
meikaik <meikaikoh@gmail.com>
2017-05-31 22:57:27 -0400

    Moved Marmoset class to a new file for testing

---
+++ test/test_marmoset_submit.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'mechanize'
+require 'commander'
+require 'choice'
+require 'cli_options'
+require 'marmoset'
+
+class MarmosetTest < Minitest::Test
+  # ADD TESTS HERE
+end
